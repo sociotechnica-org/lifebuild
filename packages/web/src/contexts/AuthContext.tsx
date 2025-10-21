c25ed656454389aeae7f717c3b967ba08f55912a
Jess Martin <jessmartin@gmail.com>
2025-10-21 16:42:32 -0400

    Fix lint errors

--- packages/web/src/contexts/AuthContext.tsx
+++ packages/web/src/contexts/AuthContext.tsx
@@ -69,6 +69,10 @@ const usersEqual = (a: AuthUser | null, b: AuthUser | null) => {
     const instanceA = aInstances[i]
     const instanceB = bInstances[i]
 
+    if (!instanceA || !instanceB) {
+      return false
+    }
+
     if (
       instanceA.id !== instanceB.id ||
       instanceA.name !== instanceB.name ||
