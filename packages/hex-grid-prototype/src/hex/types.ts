b16b13481f441ff4583a1eeef2e81e7894b22aec
Jess Martin <jessmartin@gmail.com>
2026-02-21 07:16:59 -0500

    Deduplicate prototype hex utilities

--- packages/hex-grid-prototype/src/hex/types.ts
+++ packages/hex-grid-prototype/src/hex/types.ts
@@ -1,11 +1,1 @@
-/** Cube coordinate (q + r + s = 0) */
-export interface HexCoord {
-  q: number
-  r: number
-  s: number
-}
-
-export interface HexCell {
-  coord: HexCoord
-  key: string
-}
+export type { HexCell, HexCoord } from '@lifebuild/shared/hex'
